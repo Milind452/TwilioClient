0f5e27af832aca3064e4ff54ad8d16989a90d028
Milind452 <mohapatra.milind@gmail.com>
2022-11-18 22:43:54 +0530

    Call smsService from controller

--- TwilioClient.API/Controllers/SMS/SMSController.cs
+++ TwilioClient.API/Controllers/SMS/SMSController.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using TwilioClient.Application.Interfaces;
+using TwilioClient.Application.Models;
 
 namespace TwilioClient.API.Controllers
 {
     public class SMSController : ApiControllerBase
     {
-        [HttpPost]
-        public async Task<ActionResult> SendSMS()
+        private readonly ISMSService _smsService;
+
+        public SMSController(ISMSService smsService)
         {
+            _smsService = smsService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SendSMS(SMSModel smsModel)
+        {
+            // #TODO: Add auth service to authenticate calling app before saving sms
+            await _smsService.SaveSMS(smsModel);
             return Ok();
         }
     }
