69b3760950fa42f4fe75e3563e2e382cd1577d45
Milind452 <mohapatra.milind@gmail.com>
2022-11-19 17:51:55 +0530

    Make from number manadatory

--- TwilioClient.API/Validators/SMSModelValidator.cs
+++ TwilioClient.API/Validators/SMSModelValidator.cs
@@ -9,6 +9,7 @@ namespace TwilioClient.API.Validators
         {
             RuleFor(m => m.AppName).NotEmpty().MaximumLength(100);
             RuleFor(m => m.AppToken).NotEmpty().MaximumLength(100);
+            RuleFor(m => m.From).NotEmpty().MaximumLength(100);
             RuleFor(m => m.To).NotEmpty().MaximumLength(100);
             RuleFor(m => m.Body).NotEmpty();
         }
