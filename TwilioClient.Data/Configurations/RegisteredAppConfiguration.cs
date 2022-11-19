d31db878b2d57f828449d68fcbcadd71373168ff
Milind452 <mohapatra.milind@gmail.com>
2022-11-19 18:17:18 +0530

    Add configuration for registeredApp

---
+++ TwilioClient.Data/Configurations/RegisteredAppConfiguration.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TwilioClient.Core.Entities;
+
+namespace TwilioClient.Data.Configurations
+{
+    public class
+    RegisteredAppConfiguration
+    : IEntityTypeConfiguration<RegisteredApp>
+    {
+        public void Configure(EntityTypeBuilder<RegisteredApp> builder)
+        {
+            builder.Property(b => b.AppName).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.AppToken).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.TwilioSID).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.TwilioToken).IsRequired().HasMaxLength(100);
+        }
+    }
+}
