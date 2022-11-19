bdaf1152899896f53ff8a6cca0e6af193c35f54f
Milind452 <mohapatra.milind@gmail.com>
2022-11-19 19:51:45 +0530

    Register dbContext and Configuration in program.cs

--- TwilioClient.API/Program.cs
+++ TwilioClient.API/Program.cs
@@ -1,7 +1,9 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 using TwilioClient.API.Validators;
 using TwilioClient.Common.Extensions;
+using TwilioClient.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,18 @@ builder.Services.RegisterServices();
 builder.Services.AddFluentValidationAutoValidation();
 // Register all validators
 builder.Services.AddValidatorsFromAssemblyContaining<SMSModelValidator>();
+
+// Add configuration with appsettings.json file
+IConfiguration configuration = new ConfigurationBuilder()
+   .AddJsonFile("appsettings.json", true, true)
+   .Build();
+
+// Register AppDbContext
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+        options.UseSqlServer(
+            configuration.GetConnectionString("DefaultConnection"),
+            x => x.MigrationsAssembly("WebApplication1.Migrations")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
