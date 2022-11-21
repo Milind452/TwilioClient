b29ce9393717ce829f28dbb7739949e5d4ae5b88
Milind452 <mohapatra.milind@gmail.com>
2022-11-21 17:22:40 +0530

    Add configuration for outboundSms

---
+++ TwilioClient.Data/Configurations/OutboundSMSConfiguration.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TwilioClient.Core.Entities;
+
+namespace TwilioClient.Data.Configurations
+{
+    public class
+    OutboundSMSConfiguration
+    : IEntityTypeConfiguration<OutboundSMS>
+    {
+        public void Configure(EntityTypeBuilder<OutboundSMS> builder)
+        {
+            builder.Property(b => b.Id).ValueGeneratedOnAdd();
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.AppName).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.TwilioSID).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.Body).IsRequired();
+            builder.Property(b => b.To).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.From).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.ReceivedUTC).IsRequired();
+            builder.Property(b => b.SentUTC);
+            builder.Property(b => b.Status).HasConversion<string>();
+            builder.Property(b => b.MessageResponse);
+        }
+    }
+}
