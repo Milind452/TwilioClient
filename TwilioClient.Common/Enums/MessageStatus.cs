85f7292e33898a31c7b50f0b1849531c240d4d08
Milind452 <mohapatra.milind@gmail.com>
2022-11-21 17:16:10 +0530

    Add OutboundSms entity

---
+++ TwilioClient.Common/Enums/MessageStatus.cs
@@ -0,0 +1,10 @@
+namespace TwilioClient.Common.Enums
+{
+    public enum MessageStatus
+    {
+        None = 0,
+        Pending = 1,
+        Successful = 2,
+        Failed = 3
+    }
+}
---
+++ TwilioClient.Core/Entities/OutboundSMS.cs
@@ -0,0 +1,29 @@
+using TwilioClient.Common.Enums;
+
+namespace TwilioClient.Core.Entities
+{
+    public class OutboundSMS
+    {
+        public long Id { get; set; }
+
+        public string AppName { get; set; }
+
+        public string TwilioSID { get; set; }
+
+        public string TwilioToken { get; set; }
+
+        public string Body { get; set; }
+
+        public string To { get; set; }
+
+        public string From { get; set; }
+
+        public DateTime? ReceivedUTC { get; set; }
+
+        public DateTime? SentUTC { get; set; }
+
+        public MessageStatus Status { get; set; }
+
+        public string MessageResponse { get; set; }
+    }
+}
