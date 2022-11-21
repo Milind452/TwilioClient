360d55219fe7031529bf0b561fe961b79e48406f
Milind452 <mohapatra.milind@gmail.com>
2022-11-21 17:41:47 +0530

    Add outboundSms migration

---
+++ TwilioClient.Data/Migrations/20221121120938_ADD_OutboundSMS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TwilioClient.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ADDOutboundSMS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OutboundSMS",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    TwilioSID = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    TwilioToken = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    To = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    From = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ReceivedUTC = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()"),
+                    SentUTC = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MessageResponse = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExternalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboundSMS", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutboundSMS");
+        }
+    }
+}
