2b78e5283855d384518de907995e733f0bd42a10
Milind452 <mohapatra.milind@gmail.com>
2022-11-22 22:15:31 +0530

    Add migration for send grid api key

---
+++ TwilioClient.Data/Migrations/20221122154134_Add_SendGridKey.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TwilioClient.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSendGridKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SendGridAPIKey",
+                table: "RegisteredApp",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SendGridAPIKey",
+                table: "RegisteredApp");
+        }
+    }
+}
