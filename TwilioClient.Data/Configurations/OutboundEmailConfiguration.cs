a61d41bf39f9b2f74dff433fbcf0c2c49a6ec639
Milind452 <mohapatra.milind@gmail.com>
2022-11-23 17:24:51 +0530

    Add configuration for outbound email entity

---
+++ TwilioClient.Data/Configurations/OutboundEmailConfiguration.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using TwilioClient.Core.Entities;
+
+namespace TwilioClient.Data.Configurations
+{
+    public class
+    OutboundEmailConfiguration
+    : IEntityTypeConfiguration<OutboundEmail>
+    {
+        public void Configure(EntityTypeBuilder<OutboundEmail> builder)
+        {
+            builder.Property(b => b.Id).ValueGeneratedOnAdd();
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.AppName).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.SendGridAPIKey).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.Body).IsRequired();
+            builder.Property(b => b.Subject).IsRequired();
+            builder.Property(b => b.To).IsRequired().HasMaxLength(100);
+            builder.Property(b => b.From).IsRequired().HasMaxLength(100);
+            builder
+                .Property(b => b.ReceivedUTC)
+                .IsRequired()
+                .HasDefaultValueSql("getutcdate()");
+            builder.Property(b => b.SentUTC);
+            builder
+                .Property(b => b.Status)
+                .HasConversion<string>()
+                .HasMaxLength(100);
+            builder.Property(b => b.MessageResponse);
+            builder
+                .Property(b => b.ExternalId)
+                .IsRequired()
+                .ValueGeneratedOnAdd()
+                .HasValueGenerator<SequentialGuidValueGenerator>();
+        }
+    }
+}
