2ab856a6ba82b49bd4398f8b7ee4191a6091117e
Milind452 <mohapatra.milind@gmail.com>
2023-01-10 21:09:09 +0530

    Add MessageSID to outbound sms

---
+++ TwilioClient.Data/Migrations/20230110153834_Add_MessageSID.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TwilioClient.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMessageSID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MessageSID",
+                table: "OutboundSMS",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MessageSID",
+                table: "OutboundSMS");
+        }
+    }
+}
